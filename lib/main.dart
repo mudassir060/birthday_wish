5a4fd602353ac68e8db51cf5cd6aa9bf18945322
mudassir060 <64356441+mudassir060@users.noreply.github.com>
2026-01-09 19:47:24 +0500

    update code

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,23 @@
-// ignore_for_file: avoid_print
-
 import 'dart:async';
-import 'dart:math';
+import 'dart:developer';
+import 'dart:math' hide log;
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_background_service/flutter_background_service.dart';
-import 'package:hbd/app/app.bottomsheets.dart';
-import 'package:hbd/app/app.locator.dart';
-import 'package:wallpaper/wallpaper.dart';
 import 'package:just_audio/just_audio.dart';
+import 'package:wallpaper/wallpaper.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  print("1");
-  await setupLocator();
-  // setupDialogUi();
-  setupBottomSheetUi();
   runApp(const MainApp());
-  background();
+
+  await initializeService();
 }
 
-Future<void> background() async {
+Future<void> initializeService() async {
   final service = FlutterBackgroundService();
-  print("2");
+
   await service.configure(
     androidConfiguration: AndroidConfiguration(
       onStart: onStart,
@@ -34,74 +27,68 @@ Future<void> background() async {
       initialNotificationTitle: 'TrekQuest Service',
       initialNotificationContent: 'Background service running',
       foregroundServiceNotificationId: 999,
+      // Ensure you have a valid small icon in drawable
+      // Default is "ic_notification"
+      // notificationIcon: 'ic_notification',
     ),
     iosConfiguration: IosConfiguration(),
   );
+
+  service.startService();
 }
 
 @pragma('vm:entry-point')
-Future<void> onStart(service) async {
-  print("3");
+Future<void> onStart(ServiceInstance service) async {
   DartPluginRegistrant.ensureInitialized();
-  print("Runing in background");
-  WidgetsFlutterBinding.ensureInitialized();
 
-  bool isMusicPlay = true;
+  if (service is AndroidServiceInstance) {
+    service.setForegroundNotificationInfo(
+      title: "TrekQuest Service",
+      content: "Background service is running",
+    );
+  }
+
   Timer.periodic(const Duration(seconds: 15), (timer) async {
     int day = DateTime.now().day;
-    print("Runing in background");
-    if (day == 1) {
-      if (isMusicPlay == true) {
-        isMusicPlay = false;
-        playMusic();
-      }
+    if (day == 9) {
+      playMusic();
       setWallpaper();
-    } else {
-      print("else");
     }
   });
 }
 
 void setWallpaper() async {
   List<String> images = [
-    "https://firebasestorage.googleapis.com/v0/b/trekquest-65676.appspot.com/o/0.png?alt=media&token=9343f286-7f6f-40cd-9bbb-d0f90bce928a",
-    "https://firebasestorage.googleapis.com/v0/b/trekquest-65676.appspot.com/o/1.png?alt=media&token=7efd2132-cc0b-443e-8455-195a8322fd52",
-    "https://firebasestorage.googleapis.com/v0/b/trekquest-65676.appspot.com/o/2.png?alt=media&token=1ea1af90-5d95-495c-b253-afbec5a8a5b7",
-    "https://firebasestorage.googleapis.com/v0/b/trekquest-65676.appspot.com/o/3.png?alt=media&token=3cfda5b7-5776-4010-8573-2c68609b7936",
-    "https://firebasestorage.googleapis.com/v0/b/trekquest-65676.appspot.com/o/4.png?alt=media&token=f4a57c3e-2e3c-4ca2-9250-e7806c709293",
+    "https://firebasestorage.googleapis.com/v0/b/trekquest-65676.appspot.com/o/0.png?alt=media",
+    "https://firebasestorage.googleapis.com/v0/b/trekquest-65676.appspot.com/o/1.png?alt=media",
+    "https://firebasestorage.googleapis.com/v0/b/trekquest-65676.appspot.com/o/2.png?alt=media",
   ];
 
-  Random random = Random();
-  String randomImageUrl = images[random.nextInt(images.length)];
+  final randomImage = images[Random().nextInt(images.length)];
 
   try {
-    WidgetsFlutterBinding.ensureInitialized();
-    Stream<String> progressString =
-        Wallpaper.imageDownloadProgress(randomImageUrl);
-    progressString.listen((data) async {
-      print("DataReceived: $data");
+    Stream<String> progress = Wallpaper.imageDownloadProgress(randomImage);
+    progress.listen((data) {
+      log("Download Progress: $data");
     }, onDone: () async {
       await Wallpaper.bothScreen(
         options: RequestSizeOptions.resizeExact,
       );
-      print("Task Done");
-    }, onError: (error) {
-      print("Some Error $error");
+      log("Wallpaper Set!");
     });
   } catch (e) {
-    print("========>$e");
+    log("Error setting wallpaper: $e");
   }
 }
 
-playMusic() async {
+void playMusic() async {
   final player = AudioPlayer();
   try {
-    await player.setAsset('assets/icons/Desi Birthday Anthem.mp3');
-    player.load;
+    await player.setAsset('assets/Desi Birthday Anthem.mp3');
     await player.setVolume(0.5);
     player.play();
   } catch (e) {
-    print("Error playing audio: $e");
+    log("Error playing audio: $e");
   }
 }
 
@@ -111,19 +98,9 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // initialRoute: Routes.startupView,
-      // onGenerateRoute: StackedRouter().onGenerateRoute,
-      // navigatorKey: StackedService.navigatorKey,
-      // navigatorObservers: [
-      //   StackedService.routeObserver,
-      // ],
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('TrekQuest Background Service'),
-        ),
-        body: const Center(
-          child: Text('Background service is running...'),
-        ),
+        appBar: AppBar(title: const Text('TrekQuest Background Service')),
+        body: const Center(child: Text('Background service is running...')),
       ),
     );
   }
